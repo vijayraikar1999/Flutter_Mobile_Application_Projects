5216c0e5122e22d885f3a2bd807c895cc3d47024
vijay <vijayraikar1999@gmail.com>
2021-05-04 01:35:14 +0530

    flutter_auth app in progress

---
+++ flutter_auth/lib/screens/components/body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'background.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+      child: Column(),
+    );
+  }
+}
