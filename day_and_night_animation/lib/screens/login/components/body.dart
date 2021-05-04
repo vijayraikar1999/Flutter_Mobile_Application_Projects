5e17e34976493fe7622f8de9106044a94672a39c
vijay <vijayraikar1999@gmail.com>
2021-05-05 02:13:18 +0530

    day_night_animation flutter app completed

--- day_and_night_animation/lib/screens/login/components/body.dart
+++ day_and_night_animation/lib/screens/login/components/body.dart
@@ -12,31 +12,84 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  bool _isFullSun = false;
+  bool _isDayMode = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 1), () {
+      setState(() {
+        _isFullSun = true;
+      });
+    });
+  }
+
+  void changeMode(int activeTab) {
+    if (activeTab == 0) {
+      // Then it's day
+      setState(() {
+        _isDayMode = true;
+      });
+      // At first it sets isDayMode to true and then after 300 milliseconds it sets isFullSun to true.
+      Future.delayed(Duration(milliseconds: 300), () {
+        setState(() {
+          _isFullSun = true;
+        });
+      });
+    } else {
+      setState(() {
+        _isFullSun = false;
+      });
+      Future.delayed(Duration(milliseconds: 300), () {
+        setState(() {
+          _isDayMode = false;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     List<Color> lightBgColors = [
       Color(0xFF8C2480),
       Color(0xFFCE587D),
       Color(0xFFFF9485),
+      // Add more light on background
+      if (_isFullSun) Color(0xFFFF9D80),
     ];
 
-    return Container(
+    // Bg colors for night mode
+
+    List<Color> darkBGColors = [
+      Color(0xFF8c2480),
+      Color(0xFF283584),
+      Color(0xFF376AB2),
+    ];
+
+    Duration _duration = Duration(seconds: 1);
+
+    return AnimatedContainer(
+      duration: _duration,
       width: double.infinity,
       height: SizeConfig.screenHeight,
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: lightBgColors,
+          colors: _isDayMode ? lightBgColors : darkBGColors,
         ),
       ),
       child: Stack(
         children: [
           //Sun
-          Positioned(
+          AnimatedPositioned(
+            curve: Curves.easeInOut,
+            duration: _duration,
             left: getProportionateScreenWidth(30),
-            bottom: getProportionateScreenWidth(-45),
+            bottom: getProportionateScreenWidth(_isFullSun ? -45 : -120),
             child: SvgPicture.asset("assets/icons/Sun.svg"),
+            // The Sun is an SVG which uses gradient colors, that's why dark colors comes when you click on textField.
           ),
           // Land
           Land(),
@@ -48,7 +101,9 @@ class _BodyState extends State<Body> {
                 children: [
                   VerticalSpacing(of: 50),
                   Tabs(
-                    press: (value) {},
+                    press: (value) {
+                      changeMode(value);
+                    },
                   ),
                   VerticalSpacing(),
                   Text(
@@ -81,4 +136,4 @@ class _BodyState extends State<Body> {
   }
 }
 
-// Tab, AnimatedContainer,
+// Tab, AnimatedContainer, AnimatedPositioned
