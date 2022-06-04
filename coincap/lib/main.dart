8d3dbf02885acd14529540cbe2b49bd578340132
vijayraikar1999 <vijayraikar1999@gmail.com>
2022-06-04 15:12:28 +0530

    CoinCap app in progress

---
+++ coincap/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:coincap/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'CoinCap',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color.fromRGBO(88, 60, 197, 1.0),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
