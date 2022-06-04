f19e3b6eb1f4f0d1e9f34dc3602013db42479048
vijayraikar1999 <vijayraikar1999@gmail.com>
2022-06-04 23:16:53 +0530

    CoinCap app in progress

--- coincap/lib/pages/home_page.dart
+++ coincap/lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:coincap/services/http_service.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +12,147 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double? _deviceHeight, _deviceWidth;
+
+  HTTPService? _http;
+
+  @override
+  void initState() {
+    super.initState();
+    _http = GetIt.instance.get<HTTPService>();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _selectedCoinDropdown(),
+              _dataWidgets(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _selectedCoinDropdown() {
+    List<String> _coins = ['bitcoin'];
+    List<DropdownMenuItem<String>> _items = _coins
+        .map(
+          (e) => DropdownMenuItem(
+            value: e,
+            child: Text(
+              e,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        )
+        .toList();
+    return DropdownButton(
+      value: _coins.first,
+      items: _items,
+      onChanged: (_value) {},
+      dropdownColor: const Color.fromRGBO(83, 88, 206, 1.0),
+      iconSize: 30.0,
+      icon: const Icon(
+        Icons.arrow_drop_down_sharp,
+        color: Colors.white,
+      ),
+      underline: Container(),
+    );
+  }
+
+  Widget _dataWidgets() {
+    return FutureBuilder(
+      future: _http!.get('/coins/bitcoin'),
+      builder: (_context, _snapshot) {
+        if (_snapshot.hasData) {
+          Map _data = jsonDecode(_snapshot.data.toString());
+
+          num _usdPrice = _data['market_data']['current_price']['usd'];
+          num _change24h = _data['market_data']['price_change_percentage_24h'];
+
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _coinImageWidget(_data['image']['large']),
+              _currentPriceWidget(_usdPrice),
+              _percentageChangeWidget(_change24h),
+              _descriptionCardWidget(_data['description']['en']),
+            ],
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          );
+        }
+      },
+    );
+  }
+
+  Widget _currentPriceWidget(num _rate) {
+    return Text(
+      '${_rate.toStringAsFixed(2)} USd',
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 30.0,
+        fontWeight: FontWeight.w400,
+      ),
+    );
+  }
+
+  Widget _percentageChangeWidget(num _change) {
+    return Text(
+      '${_change.toString()} %',
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 15.0,
+        fontWeight: FontWeight.w300,
+      ),
+    );
+  }
+
+  Widget _coinImageWidget(String _imgURL) {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: _deviceHeight! * 0.02),
+      height: _deviceHeight! * 0.15,
+      width: _deviceWidth! * 0.15,
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: NetworkImage(_imgURL),
+        ),
+      ),
+    );
+  }
+
+  Widget _descriptionCardWidget(String _description) {
+    return Container(
+      height: _deviceHeight! * 0.45,
+      width: _deviceWidth! * 0.90,
+      margin: EdgeInsets.symmetric(vertical: _deviceHeight! * 0.05),
+      padding: EdgeInsets.symmetric(
+          vertical: _deviceHeight! * 0.01, horizontal: _deviceHeight! * 0.01),
+      color: const Color.fromRGBO(83, 88, 206, 0.5),
+      child: Text(
+        _description,
+        style: const TextStyle(color: Colors.white),
+      ),
+    );
   }
 }
--- coincap/lib/services/http_service.dart
+++ coincap/lib/services/http_service.dart
@@ -12,4 +12,15 @@ class HTTPService {
     _appConfig = GetIt.instance.get<AppConfig>();
     _baseUrl = _appConfig!.COIN_API_BASE_URL;
   }
+
+  Future<Response?> get(String _path) async {
+    try {
+      String _url = '$_baseUrl$_path';
+      Response _response = await dio.get(_url);
+      return _response;
+    } catch (e) {
+      print('HTTPService: Unable to perform a get request');
+      print(e);
+    }
+  }
 }
