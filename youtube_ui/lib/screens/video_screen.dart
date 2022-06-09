cc9f3f72892bf43adec57495ff01c7ace8efbb2d
vijayraikar1999 <vijayraikar1999@gmail.com>
2022-06-10 00:34:36 +0530

    YouTube UI app completed

--- youtube_ui/lib/screens/video_screen.dart
+++ youtube_ui/lib/screens/video_screen.dart
@@ -5,63 +5,98 @@ import 'package:youtube_ui/data.dart';
 import 'package:youtube_ui/screens/nav_screen.dart';
 import 'package:youtube_ui/widgets/widgets.dart';
 
-class VideoScreen extends StatelessWidget {
+class VideoScreen extends StatefulWidget {
   const VideoScreen({Key? key}) : super(key: key);
 
   @override
+  State<VideoScreen> createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  ScrollController? _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Theme.of(context).scaffoldBackgroundColor,
-        child: CustomScrollView(
-          shrinkWrap: true,
-          slivers: [
-            SliverToBoxAdapter(
-              child: Consumer(
-                builder: (context, watch, _) {
-                  final selectedVideo = watch(selectedVideoProvider).state;
-                  return SafeArea(
-                    child: Column(
-                      children: [
-                        Stack(
-                          children: [
-                            Image.network(
-                              selectedVideo!.thumbnailUrl,
-                              height: 220.0,
-                              width: double.infinity,
-                              fit: BoxFit.cover,
-                            ),
-                            IconButton(
-                              iconSize: 30.0,
-                              onPressed: () => context
-                                  .read(miniPlayerControllerProvider)
-                                  .state
-                                  .animateToHeight(state: PanelState.MIN),
-                              icon: const Icon(Icons.keyboard_arrow_down),
-                            )
-                          ],
-                        ),
-                        const LinearProgressIndicator(
-                          value: 0.4,
-                          valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
-                        ),
-                        VideoInfo(video: selectedVideo),
-                      ],
-                    ),
-                  );
-                },
+    return GestureDetector(
+      onTap: () => context
+          .read(miniPlayerControllerProvider)
+          .state
+          .animateToHeight(state: PanelState.MAX),
+      child: Scaffold(
+        body: Container(
+          color: Theme.of(context).scaffoldBackgroundColor,
+          child: CustomScrollView(
+            controller: _scrollController,
+            shrinkWrap: true,
+            slivers: [
+              SliverToBoxAdapter(
+                child: Consumer(
+                  builder: (context, watch, _) {
+                    final selectedVideo = watch(selectedVideoProvider).state;
+                    return SafeArea(
+                      child: Column(
+                        children: [
+                          Stack(
+                            children: [
+                              Image.network(
+                                selectedVideo!.thumbnailUrl,
+                                height: 220.0,
+                                width: double.infinity,
+                                fit: BoxFit.cover,
+                              ),
+                              IconButton(
+                                iconSize: 30.0,
+                                onPressed: () => context
+                                    .read(miniPlayerControllerProvider)
+                                    .state
+                                    .animateToHeight(state: PanelState.MIN),
+                                icon: const Icon(Icons.keyboard_arrow_down),
+                              )
+                            ],
+                          ),
+                          const LinearProgressIndicator(
+                            value: 0.4,
+                            valueColor:
+                                AlwaysStoppedAnimation<Color>(Colors.red),
+                          ),
+                          VideoInfo(video: selectedVideo),
+                        ],
+                      ),
+                    );
+                  },
+                ),
               ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  final video = suggestedVideos[index];
-                  return VideoCard(video: video, hasPadding: true);
-                },
-                childCount: suggestedVideos.length,
+              SliverList(
+                delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                    final video = suggestedVideos[index];
+                    return VideoCard(
+                      video: video,
+                      hasPadding: true,
+                      onTap: () => _scrollController!.animateTo(
+                        0,
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.easeIn,
+                      ),
+                    );
+                  },
+                  childCount: suggestedVideos.length,
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
